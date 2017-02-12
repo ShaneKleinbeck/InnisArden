285bf297c6e89d073be49d5d6c4ed519f753394a
Shane Kleinbeck <shanekleinbeck@gmail.com>
2017-02-12 18:16:24 -0500

    Completed Contact Us Form

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,31 @@
 
 console.log('CONNECTED');
 
+var photos = ['/public/media/award1.jpg', '/public/media/award2.jpg', '/public/media/award3.jpg', '/public/media/live-music.jpg', '/public/media/theStore1.jpg', '/public/media/theStore2.jpg', '/public/media/theStore3.jpg', '/public/media/theStore4.jpg'];
+
 $(document).ready(function(){
+
+  // Implement gallery
+  for(var i = 0; i < photos.length; i++){
+    $('.galleryGrid').append('<div class="col-xs-3"><a href="" data-toggle="modal"><img src="' + photos[i] + '" alt="" class="gridImg"></a></div>');
+  }
+
+  // Toggle Form
+  var formCount = 0;
+  $('#toggleInput').click(function(){
+    console.log('formToggled');
+    formCount++;
+    if(formCount % 2 === 0){
+      $('.jumbotron').css({"display": "none"});
+    } else {
+      $('.jumbotron').css({"display": "block"});     
+    }
+  });
+
+
   // Smooth Scroll Functionality
   smoothScroll.init({
-      selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+      selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)ßß
       selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
       speed: 500, // Integer. How fast to complete the scroll in milliseconds
       easing: 'easeInOutCubic', // Easing pattern to use
@@ -32,5 +53,7 @@ $(document).ready(function(){
     $('.hide-overlay').toggleClass('video-nav-overlay');
   })
 });
+
+
 
 
